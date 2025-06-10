dde53f9a4255b046be1d80945429b31ae6f05493
gyeongyeon <gjeon0321@gmail.com>
2025-06-11 02:02:27 +0900

    fix: sitemap.ts
    sitemap 에 태그 정보도 추가

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,27 @@ import { getBlogPosts } from "app/blog/utils";
 export const baseUrl = "https://gimbap.dev";
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
+  const posts = getBlogPosts();
+
+  const blogs = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ["", "/blog"].map((route) => ({
+  const tagSet = new Set<string>();
+  posts.forEach((post) => {
+    post.metadata.tags?.forEach((tag) => tagSet.add(tag));
+  });
+
+  const tagPages = [...tagSet].map((tag) => ({
+    url: `${baseUrl}/tags/${encodeURIComponent(tag)}`,
+    lastModified: new Date().toISOString().split("T")[0], // or latest related post date
+  }));
+
+  const staticRoutes = ["", "/blog", "/tags"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...staticRoutes, ...blogs, ...tagPages];
 }
