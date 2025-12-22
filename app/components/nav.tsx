ba29015eec397bee72d75a7705280f4d55748ac8
gyeongyeon <gjeon0321@gmail.com>
2025-12-22 18:35:45 +0900

    style: 테마 아이콘 영역이 늦게 로드됨에 따른 ui 변화가 있어 높이 고정

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -15,15 +15,15 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <nav className="mb-12 pb-6 border-b border-neutral-200 dark:border-neutral-800">
-      <div className="flex flex-row items-center justify-between">
+    <nav className="pb-6 mb-12 border-b border-neutral-200 dark:border-neutral-800">
+      <div className="flex flex-row items-center justify-between h-10">
         <div className="flex flex-row gap-6">
           {Object.entries(navItems).map(([path, { name }]) => {
             return (
               <Link
                 key={path}
                 href={path}
-                className="text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 transition-colors duration-200 text-sm font-medium"
+                className="text-sm font-medium transition-colors duration-200 text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100"
               >
                 {name}
               </Link>
